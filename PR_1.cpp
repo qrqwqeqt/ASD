0c5cf5062a1ce9c4c8ec78580e71d7f66a8af42c
qrqwqeqt <114399108+qrqwqeqt@users.noreply.github.com>
2023-03-06 12:48:31 +0200

    Add files via upload

---
+++ PR_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+
+float calculate(float x, int k, float M, int i);
+
+
+
+int main()
+{
+    int i;
+    float x, calculated, M;
+
+    printf("Enters two numbers: ");
+    scanf_s("%f %d %f", &x, &i, &M);
+    calculated = calculate(x, 2, M, i);
+    printf("calculated = %f", calculated);
+    return 0;
+}
+
+float calculate(float x, int k, float M, int i)
+{   
+
+    float F;
+    if (k <= i)
+    {
+        M *= -1 * (x * (2 * k - 3) / (2 * k - 2));
+        k++;
+        F = calculate(x, k, M, i); 
+    }
+    else 
+    {
+        F = 1.0 - M;
+    }
+    F += M;
+    return F;
+}   
